314ff1746c9faac9c74d22da0195db3ed51d6ac6
Elijah Noble <elijahnobledev@gmail.com>
2022-04-08 18:54:29 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -28,21 +28,41 @@ const nestedMuppet = {
 // Strings
 
 // 1. Use destructuring to assign appropriate variables based on the sounds animals make.
+    const animalSounds = {
+      moo,
+      neigh,
+      baa,
+      oink,
+      cluck
+    }
+
+    const {moo='cow', neigh='horse', baa='sheep', oink='pig', cluck='chicken'} = animalSounds
 
 // 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
+    const animalNames = {}
+    const {bessie='cow', dolly='sheep', babe='pig', little='chicken'} = animalNames
 
 // 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
+    const animalColors = {}
+    const {blackAndWhite='cow', black='sheep', pink='pig'} = animalColors
 
 // Arrays
 
-// 4. Use destructuring to assign appropriate variables using the color names.
+// 4. Use destructuring to assign appropriate variables using the color 
+    const rainbowColors = []
+    const [red='red', orange='orange', yellow='yellow', green='green', blue='blue', indigo='indigo', violet='violet'] = rainbowColors
 
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
+    const [r='red', o='orange', y='yellow', g='green', b='blue', i='indigo', v='violet'] = rainbowColors
 
 // 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
+    const [indg='indigo'] = rainbowColors
 
 // Objects
 
 // 7. Use destructuring to assign all variables using the keys as the variable names
+    const {muppetName, color, song, job, partner} = muppet
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+    const {song2, song4} = nestedMuppet.album.theMuppetMovie
+    const {nestedJob, nestedPartner} = nestedMuppet
